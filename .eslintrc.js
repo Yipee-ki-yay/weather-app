91b5bdd81b3a6b19a3a370cc6c068ad953f6ed9b
Alexander Morozov <alexander.morozov@shijigroup.com>
2023-05-09 12:49:22 +0300

    implemente weather app

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "@react-native-community",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    semi: ["error", "never"],
+    "coma-dangle": [2, "never"],
+    "react/jsx-filename-extantion": [1, { extensions: [".js", ".jsx"] }],
+    "no-use-before-define": [
+      "error",
+      { functions: true, classes: true, variables: false },
+    ],
+  },
+};
